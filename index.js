cf39b61228ad23d0cfdf9177badab8eed998fc80
Anne Nabwire <nabanne96@gmail.com>
2025-01-14 09:57:55 +0300

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'];
+function findMatching(drivers,name){
+    return drivers.filter(driver =>driver.toLowerCase()===name.toLowerCase());
+}
+ findMatching(drivers,'Bobby');
+
+ function fuzzyMatch(drivers, letters){
+    return drivers.filter(driver=>driver.startsWith(letters));
+
+ }
+ fuzzyMatch(drivers, 'Sa')
+
+ const drivers1 = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+ function matchName(driversArray, name){
+      return driversArray.filter(driver =>driver.name===name);
+ }
+
+ matchName(drivers1,'Bobby');
